e88f12e40e5c2a922647da6bbea02c5e7d34639c
Islam Madkaur <islammadkaur@gmail.com>
2020-10-10 23:37:55 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,7 +39,7 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix[0][0]
+  matrix[0][2] = "Lemons"
   matrix
 
   # Given any matrix (array of arrays), a row index and a column index,
